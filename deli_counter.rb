ca90a5a756e3d09864a674f04c44e65fd5ba02a1
Annie Wiegel <annewiegel17@gmail.com>
2021-02-04 02:10:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def line(array)
+if array = []
+puts "The line is currently empty."
+  else array.each do |name|
+    
+    puts "The line is currently: 1. Ada 2. Grace 3. Kent"
+end
+end
+end
+
+
+def take_a_number()
+  
+end
+
+
+def now_serving()
+  
+end
